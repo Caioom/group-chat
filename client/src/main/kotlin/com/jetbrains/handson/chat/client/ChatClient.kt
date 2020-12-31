9e194a48907c102202f7dc02f50fee5e24b00526
caioom <caio.ds.2003@gmail.com>
2020-12-31 15:49:03 -0300

    Client coded, send and receive messages, but not still concurrently

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -9,5 +9,23 @@ import kotlinx.coroutines.*
 
 @KtorExperimentalAPI
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
 
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            while(true) {
+                val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                println(othersMessage.readText())
+                val myMessage = readLine();
+                if(myMessage != null) {
+                    send(myMessage)
+                }
+            }
+        }
+    }
+
+    client.close()
+    println("Connection closed. Goodbye!")
 }
